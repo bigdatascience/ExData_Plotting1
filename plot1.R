d9624b0e32ddbe597bf8dd2f7acfbb2dfa745696
bigdatascience <rupesh.dan@gmail.com>
2014-05-08 21:01:18 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,6 @@
+## 
+##  Plot 1
+##
 ## Read Power Date File an assign to a variable
 power <- read.csv("household_power_consumption.txt", colClasses = "character", sep = ";")
 
@@ -9,3 +12,4 @@ powerDate <- power[power$Date %in% as.Date(c('2007-02-01','2007-02-02')), ]
 png(filename="plot1.png")
 hist(as.numeric(powerDate$Global_active_power),col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)",ylab="Frequency")
 dev.off()
+
